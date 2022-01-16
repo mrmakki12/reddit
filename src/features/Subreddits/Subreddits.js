d2c71dc91a6125c5105728c2b937dfcf13508aa2
Tyreeck M <mrmakki12@users.noreply.github.com>
2022-01-16 15:44:19 -0500

    reformatted some things

--- src/features/Subreddits/Subreddits.js
+++ src/features/Subreddits/Subreddits.js
@@ -12,16 +12,23 @@ export const Subreddits = () => {
 
     return (
         <section className="subreddits-container">
+
             <div className="subreddits-header">
                 <h1>Subreddits</h1>
             </div>
+
             {
                 subreddits.map(subreddit => {
                     return (
-                        <div key={subreddit.url} className={subreddit.url === selectedSubreddit ? 'selected subreddit' : 'subreddit'} onClick={() => dispatch(setSubreddit(subreddit.url.toString()))}>
+                        
+                        <div key={subreddit.url} 
+                            className={subreddit.url === selectedSubreddit ? 'selected subreddit' : 'subreddit'} 
+                            onClick={() => dispatch(setSubreddit(subreddit.url.toString()))}>
+
                             <div className="subreddit-image-container">
                                 <img src={subreddit.img} alt={subreddit.name + ' icon'}/>
                             </div>
+
                             <h3>{subreddit.name}</h3>
                         </div>
                     )
